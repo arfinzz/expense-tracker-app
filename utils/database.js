2e97fd210ed1115507d00383b0d1616097cfdd91
arfinzz <khanarfin2000@gmail.com>
2023-07-01 21:22:21 +0530

    READY FOR DEPLOYMENT

--- utils/database.js
+++ utils/database.js
@@ -3,7 +3,7 @@ const Sequelize=require('sequelize');
 
 const sequelize=new Sequelize(process.env.MYSQL_DB_NAME,process.env.MYSQL_USER_NAME,process.env.MYSQL_PASSWORD,{
     dialect: 'mysql',
-    host: 'localhost'
+    host: process.env.MYSQL_HOST_NAME
 });
 
 module.exports=sequelize;
